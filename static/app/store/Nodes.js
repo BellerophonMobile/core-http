5cd7646751e63ea5b167cde754a10a9cdf2a0e52
Tom Wambold <tom5760@gmail.com>
2012-05-27 19:43:38 -0400

    Disable autoSync on JavaScript stores.
    This lets us control when data is posted to the server, and better
    handle successes/failures.

--- static/app/store/Nodes.js
+++ static/app/store/Nodes.js
@@ -1,7 +1,6 @@
 Ext.define('core.store.Nodes', {
     extend: 'Ext.data.Store',
     autoLoad: false,
-    autoSync: true,
     model: 'core.model.Node',
     storeId: 'nodeStore',
 });
--- static/app/store/Sessions.js
+++ static/app/store/Sessions.js
@@ -1,7 +1,6 @@
 Ext.define('core.store.Sessions', {
     extend: 'Ext.data.Store',
     autoLoad: true,
-    autoSync: true,
     model: 'core.model.Session',
     storeId: 'sessionStore',
 });
