651ccbf59d8346013f057105f5f236aa0d106e19
Tom Wambold <tom5760@gmail.com>
2012-08-13 17:47:09 +0200

    Python CLI: Support creating/deleting/listing nodes.

--- python/core.py
+++ python/core.py
@@ -13,47 +13,88 @@ class Daemon(object):
 
     def __init__(self, address=DEFAULT_ADDRESS):
         self.address = address
+        config = {
+            'danger_mode': True,
+        }
+        headers = {
+            'Content-Type': 'application/json'
+        }
+
+        self.req = requests.session(config=config, headers=headers)
 
     def sessions(self):
-        r = requests.get(make_url(self.address, 'sessions'))
-        r.raise_for_status()
-        return [Session(self.address, json) for json in r.json]
-
-    def new_session(self, name, user):
-        data = {
-            'name': name,
-            'user': user,
-        }
-        r = post_json((self.address, 'sessions'), data)
-        r.raise_for_status()
-        return Session(self.address, r.json)
+        r = self.req.get(make_url(self.address, 'sessions'))
+        return [Session(self.address, self.req, json) for json in r.json]
 
     def get_session(self, sid):
-        r = requests.get(make_url(self.address, 'sessions', sid))
-        r.raise_for_status()
-        return Session(self.address, r.json)
+        r = self.req.get(make_url(self.address, 'sessions', sid))
+        return Session(self.address, self.req, r.json)
+
+    def new_session(self, name, user):
+        data = json_dumps({
+            'name': name,
+            'user': user,
+        })
+        r = self.req.post(make_url(self.address, 'sessions'), data=data)
+        return Session(self.address, self.req, r.json)
 
 class Session(object):
-    def __init__(self, address, json):
+    def __init__(self, address, req, json):
         self.address = address
+        self.req = req
 
-        self._sid = json['id']
+        self._sid = json['sid']
         self._name = json['name']
         self._user = json['user']
 
-    def set_name(self, name):
-        raise NotImplementedError()
-
-    def set_user(self, user):
-        raise NotImplementedError()
-
     def delete(self):
-        r = requests.delete(make_url(self.address, 'sessions', self.sid))
-        r.raise_for_status()
+        r = self.req.delete(make_url(self.address, 'sessions', self.sid))
+
+    def nodes(self):
+        r = self.req.get(make_url(self.address, 'sessions', self.sid, 'nodes'))
+        return [Node(self.address, self.req, json) for json in r.json]
+
+    def get_node(self, nid):
+        r = self.req.get(
+                make_url(self.address, 'sessions', self.sid, 'nodes', nid))
+        return Node(self.address, self.req, r.json)
+
+    def new_node(self, ntype, name, x, y, z):
+        data = json_dumps({
+            'type': ntype,
+            'name': name,
+            'x': x,
+            'y': y,
+            'z': z,
+        })
+        r = self.req.post(make_url(self.address, 'sessions', self.sid,'nodes'),
+                          data=data)
+        return Node(self.address, self.req, r.json)
 
     sid = property(lambda self: self._sid, doc='Session ID')
-    name = property(lambda self: self._name, set_name, doc='Session Name')
-    user = property(lambda self: self._user, set_user, doc='Session Owner')
+    name = property(lambda self: self._name, doc='Session Name')
+    user = property(lambda self: self._user, doc='Session Owner')
+
+class Node(object):
+    def __init__(self, address, req, json):
+        self.address = address
+        self.req = req
+
+        self._nid = json['nid']
+        self._sid = json['sid']
+        self._name = json['name']
+        self._type = json['type']
+        self._position = (json['x'], json['y'], json['z'])
+
+    def delete(self):
+        r = self.req.delete(make_url(
+            self.address, 'sessions', self.sid, 'nodes', self.nid))
+
+    sid = property(lambda self: self._sid, doc='Session ID')
+    nid = property(lambda self: self._nid, doc='Node ID')
+    name = property(lambda self: self._name, doc='Node Name')
+    ntype = property(lambda self: self._type, doc='Node Type')
+    position = property(lambda self: self._position, doc='Node coordinates')
 
 def make_url(*args, **kwargs):
     url = '/'.join(map(str, args))
@@ -61,8 +102,5 @@ def make_url(*args, **kwargs):
         url += '/'
     return url
 
-def post_json(url_parts, data, endslash=True):
-    headers = {'Content-Type': 'application/json'}
-    return requests.post(make_url(*url_parts, endslash=endslash),
-                         headers=headers,
-                         data=json.dumps(data))
+def json_dumps(x):
+    return json.dumps(x, separators=(',', ':'))
--- python/corecli.py
+++ python/corecli.py
@@ -9,20 +9,19 @@ import sys
 from core import Daemon
 
 class Cli(object):
-    @classmethod
-    def run(cls, *args):
+    def run(self, *args):
         try:
             arg = args[0]
         except IndexError:
             arg = 'help'
 
         try:
-            func = getattr(cls, 'do_' + arg)
+            func = getattr(self, 'do_' + arg)
         except AttributeError:
             print 'Invalid argument "{}"'.format(arg)
             return 1
 
-        return getattr(cls, 'do_' + arg)(*args[1:])
+        return func(*args[1:])
 
     @staticmethod
     def do_help(*args):
@@ -33,7 +32,7 @@ class Cli(object):
 
     @staticmethod
     def do_session(*args):
-        return Session.run(*args)
+        return Session().run(*args)
 
 class Session(Cli):
     @staticmethod
@@ -42,8 +41,9 @@ class Session(Cli):
             'Usage: {} session COMMAND',
             '  session help                This help',
             '  session list                List all active sessions',
-            '  session new <name> <user>   Create a new session',
-            '  session del <id>            Destroys a session',
+            '  session new NAME USER       Create a new session',
+            '  session del ID              Destroys a session',
+            '  session get ID              Interact with a particular session',
         )).format(sys.argv[0])
 
     @staticmethod
@@ -61,14 +61,84 @@ class Session(Cli):
     @staticmethod
     def do_new(name, user):
         session = Daemon().new_session(name, user)
-        print 'ID: {}'.format(session.sid)
+        print 'ID:', session.sid
 
     @staticmethod
     def do_del(sid):
         Daemon().get_session(sid).delete()
 
+    @staticmethod
+    def do_get(sid, *args):
+        return SelectedSession(sid).run(*args)
+
+class SelectedSession(Cli):
+    def __init__(self, sid):
+        self.sid = sid
+
+    def do_help(self, *args):
+        print '\n'.join((
+            'Usage: {0} session {1} OBJECT',
+            '  session {1} help            This help',
+            '  session {1} node            Manipulate nodes of this session',
+            '  session {1} link            Manipulate links of this session',
+        )).format(sys.argv[0], self.sid)
+
+    def do_node(self, *args):
+        return Node(self.sid).run(*args)
+
+    def do_link(self, *args):
+        return Link(self.sid).run(*args)
+
+class Node(Cli):
+    def __init__(self, sid):
+        self.sid = sid
+        self.session = Daemon().get_session(self.sid)
+
+    def do_help(self, *args):
+        print '\n'.join((
+            'Usage: {} session {} node COMMAND',
+            '  node help                   This help',
+            '  node list                   List all nodes',
+            '  node new TYPE NAME X Y Z    Create a new node',
+            '  node del ID                 Destroy a node',
+            '',
+            'Types: default, wlan',
+        )).format(sys.argv[0], self.sid)
+
+    def do_list(self):
+        nodes = self.session.nodes()
+        if len(nodes) == 0:
+            print 'No nodes'
+            return
+
+        print '{} nodes'.format(len(nodes))
+        print 'ID\tName\tType\tCoordinate'
+        for node in nodes:
+            print '{0}\t{1}\t{2}\t({3[0]}, {3[1]}, {3[2]})'.format(
+                    node.nid, node.name, node.ntype, node.position)
+
+    def do_new(self, ntype, name, x, y, z):
+        node = self.session.new_node(ntype, name, x, y, z)
+        print 'ID:', node.nid
+
+    def do_del(self, nid):
+        self.session.get_node(nid).delete()
+
+class Link(Cli):
+    def __init__(self, sid):
+        self.sid = sid
+
+    def do_help(self, *args):
+        print '\n'.join((
+            'Usage: {} session {} link COMMAND',
+            '  link help                   This help',
+            '  link list                   List all links',
+            '  link new <a> <b>            Create a link between two nodes',
+            '  link del <lid>              Destroy a link',
+        )).format(sys.argv[0], self.sid)
+
 def main(argv):
-    return Cli.run(*argv[1:])
+    return Cli().run(*argv[1:])
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv))
